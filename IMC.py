03a58fb8f73276c5a25dda4be8ec884006bf9856
AsenjoArnau <arnau.asenjo09@iesantpol.cat>
2025-11-10 09:05:15 +0000

    EX01_Arnau_Asenjo

---
+++ IMC.py
@@ -0,0 +1,24 @@
+nom = input("Quin és el teu nom?: ")
+
+edat = int(input("Quina és la teva edat?: "))
+
+pes = input("Quina és la teva massa?: ")
+
+altura = float(input("Quina és la teva altura?: "))
+
+print()
+
+print(nom)
+print(edat)
+print(pes)
+print(altura)
+
+print()
+print("---- Informe de salut ----")
+print(f"Nom: {nom}")
+print(f"Edat: {edat} anys")
+print(f"Pes: {pes} kg")
+print(f"Altura: {altura} m")
+
+print()
+
