a3e03d0c9021cc875d371a5b1f1a41f7d94e0c77
AsenjoArnau <arnau.asenjo09@iesantpol.cat>
2025-11-12 12:43:59 +0000

    Phyton.imc

--- IMC.py
+++ IMC.py
@@ -1,24 +1,41 @@
-nom = input("Quin és el teu nom?: ")
+nom = input("Introdueix el teu nom: ")
 
-edat = int(input("Quina és la teva edat?: "))
+edat = int(input("Introdueix la teva edat: "))
 
-pes = input("Quina és la teva massa?: ")
+pes = float(input("Introdueix la teva pes (kg): "))
 
-altura = float(input("Quina és la teva altura?: "))
-
-print()
-
-print(nom)
-print(edat)
-print(pes)
-print(altura)
+altura = float(input("Introdueix la teva altura (m): "))
 
 print()
+
 print("---- Informe de salut ----")
-print(f"Nom: {nom}")
+print(f"Nom: {nom} {type(nom)}")
 print(f"Edat: {edat} anys")
-print(f"Pes: {pes} kg")
+print(f"pes: {pes} kg")
 print(f"Altura: {altura} m")
 
 print()
 
+imc = pes / altura**2
+    
+print(f"Índex de massa corporal (IMC): {imc}")   
+
+if imc > 40:
+    print("Classificació: Obsesitat III")
+
+elif imc > 35:
+    print("Classificació: Obsesitat II")
+
+elif imc > 30: 
+    print("Classificació: Obsesitat I")
+
+elif imc > 25: 
+    print("Classificació: Sobrepès")
+
+elif imc > 18.5: 
+    print("Classificació: Pes normal")
+
+elif imc < 18.5: 
+    print("Classificació: Pes baix")
+
+print("--------------------------")
